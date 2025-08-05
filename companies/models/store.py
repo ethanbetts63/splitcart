4a9bad519d39aa768f94472c004ee233098e8d3e
Ethan Betts <you@example.com>
2025-08-05 13:22:13 +0200

    added address fields to store model

--- companies/models/store.py
+++ companies/models/store.py
@@ -21,6 +21,23 @@ class Store(models.Model):
         blank=True,
         help_text="The base URL of the specific store, if applicable."
     )
+    address = models.CharField(
+        max_length=255,
+        blank=True,
+        help_text="The street address of the store."
+    )
+    city = models.CharField(
+        max_length=100,
+        blank=True,
+        db_index=True,  # We will likely want to query stores by city
+        help_text="The city where the store is located."
+    )
+    state = models.CharField(
+        max_length=50,
+        blank=True,
+        db_index=True,  # And also by state
+        help_text="The state or territory of the store."
+    )
 
     class Meta:
         unique_together = ('name', 'company')
