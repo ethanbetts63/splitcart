fdea7ed0309b52e4f0a2a33169667ef8b25f2825
Ethan Betts <you@example.com>
2025-08-15 11:18:12 +0200

    checkpoint

--- api/utils/archiving_utils/__init__.py
+++ api/utils/archiving_utils/__init__.py
@@ -5,10 +5,3 @@ from .build_product_list import build_product_list
 from .save_json_file import save_json_file
 from .print_product_progress import print_product_progress
 
-__all__ = [
-    'get_stores_queryset',
-    'construct_category_hierarchy',
-    'build_product_list',
-    'save_json_file',
-    'print_product_progress',
-]
--- api/utils/shop_scraping_utils/aldi/__init__.py
+++ api/utils/shop_scraping_utils/aldi/__init__.py
@@ -1,7 +1,6 @@
 from .drange import drange
 from .load_existing_stores import load_existing_stores
 from .load_progress import load_progress
-from .organize_aldi_stores import organize_aldi_stores  
 from .print_progress_bar import print_progress_bar
 from .save_progress import save_progress
 from .save_stores_incrementally import save_stores_incrementally
