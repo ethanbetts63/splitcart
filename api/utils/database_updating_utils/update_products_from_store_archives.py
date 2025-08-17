66ae5f616dbf9373531796c6c132accf8973cf91
Ethan Betts <you@example.com>
2025-08-17 21:58:52 +0200

    reset django project robust.ps1

--- api/utils/database_updating_utils/update_products_from_store_archives.py
+++ api/utils/database_updating_utils/update_products_from_store_archives.py
@@ -54,16 +54,10 @@ def update_products_from_store_archives(command):
                 if not category_paths:
                     continue
                 
-                product_obj = None
-                created = False
-                for i, category_path in enumerate(category_paths):
-                    category_obj = get_or_create_category_hierarchy(category_path, store.company)
-                    if i == 0: # Call get_or_create_product only on the first category path
-                        product_obj, created = get_or_create_product(product_data, store, category_obj, product_cache, new_products_to_create)
-                    elif product_obj and category_obj:
-                        if not hasattr(product_obj, 'categories_to_add'):
-                            product_obj.categories_to_add = set()
-                        product_obj.categories_to_add.add(category_obj.id)
+                category_path = category_paths[0]
+                category_obj = get_or_create_category_hierarchy(category_path, store.company)
+
+                product_obj, created = get_or_create_product(product_data, store, category_obj, product_cache, new_products_to_create)
                 
                 if created:
                     products_created += 1
