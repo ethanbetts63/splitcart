e400a5da30e4409578943cc82a1677c2265df8cc
Ethan Betts <you@example.com>
2025-08-30 17:27:19 +0200

    checkpoint

--- products/models/product.py
+++ products/models/product.py
@@ -46,6 +46,13 @@ class Product(models.Model):
         default=False,
         help_text="Set to True if Coles has been scraped and no barcode was found."
     )
+    plu_code = models.CharField(
+        max_length=10,
+        null=True,
+        blank=True,
+        db_index=True,
+        help_text="The Price Look-Up code for the product, typically for fresh produce."
+    )
     image_url = models.URLField(max_length=1024, blank=True, null=True)
     url = models.URLField(max_length=1024, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
