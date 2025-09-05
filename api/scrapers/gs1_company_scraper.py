551c3125d27161396450150100c0e0ac17e5020e
Ethan Betts <you@example.com>
2025-09-05 11:43:05 +0200

    checkpoint

--- api/scrapers/gs1_company_scraper.py
+++ api/scrapers/gs1_company_scraper.py
@@ -27,6 +27,7 @@ class Gs1CompanyScraper:
             
             options = webdriver.ChromeOptions()
             options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
+            options.add_argument("--incognito")
             
             self.command.stdout.write("--- Initializing Selenium browser ---")
             self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
