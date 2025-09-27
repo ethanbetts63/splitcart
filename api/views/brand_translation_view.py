779f87c88eedb31fd070e6588c327a2e8e09fe0d
Ethan Betts <you@example.com>
2025-09-27 11:41:46 +0200

    checkpoint

--- api/views/brand_translation_view.py
+++
@@ -1,8 +0,0 @@
-from .base_translation_view import BaseTranslationView
-from data_management.database_updating_classes.brand_translation_table_generator import BrandTranslationTableGenerator
-
-class BrandTranslationView(BaseTranslationView):
-    """
-    Serves the brand name translation table.
-    """
-    generator_class = BrandTranslationTableGenerator
--- api/views/product_translation_view.py
+++
@@ -1,8 +0,0 @@
-from .base_translation_view import BaseTranslationView
-from data_management.database_updating_classes.product_translation_table_generator import ProductTranslationTableGenerator
-
-class ProductTranslationView(BaseTranslationView):
-    """
-    Serves the product name translation table.
-    """
-    generator_class = ProductTranslationTableGenerator
