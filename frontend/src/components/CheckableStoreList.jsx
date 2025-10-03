345e0dc6ea340671270a356ac95603c75f24f9bb
Ethan Betts <you@example.com>
2025-10-03 17:05:57 +0200

    checkpoint

--- frontend/src/components/CheckableStoreList.jsx
+++ frontend/src/components/CheckableStoreList.jsx
@@ -18,17 +18,16 @@ const CheckableStoreList = ({ stores, selectedStoreIds, onStoreSelect }) => {
     return (
         <ListGroup>
             {stores.map(store => (
-                <ListGroup.Item key={store.id} className="d-flex justify-content-between align-items-center">
-                    <div>
-                        <Image src={companyLogos[store.company_name]} alt={`${store.company_name} logo`} height="20" className="me-2" />
-                        {`${store.store_name}`}
-                    </div>
+                <ListGroup.Item key={store.id} className="d-flex align-items-center">
                     <Form.Check 
                         type="checkbox"
                         id={`store-checkbox-${store.id}`}
                         checked={selectedStoreIds.has(store.id)}
                         onChange={() => onStoreSelect(store.id)}
+                        className="me-3"
                     />
+                    <Image src={companyLogos[store.company_name]} alt={`${store.company_name} logo`} height="20" className="me-2" />
+                    <span>{store.store_name}</span>
                 </ListGroup.Item>
             ))}
         </ListGroup>
