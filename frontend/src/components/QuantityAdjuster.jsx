6437c5ddb9400a1d13c538748848f6cb149a8426
Ethan Betts <you@example.com>
2025-10-08 21:38:50 +0200

    checkpoint

--- frontend/src/components/QuantityAdjuster.jsx
+++ frontend/src/components/QuantityAdjuster.jsx
@@ -10,7 +10,7 @@ const QuantityAdjuster = ({ quantity, onQuantityChange, className }) => {
   };
 
   return (
-    <div className={className} style={{ display: 'flex', alignItems: 'center' }}>
+    <div className={className} style={{ display: 'flex', alignItems: 'baseline' }}>
       <button onClick={handleDecrement} style={{ background: 'var(--colorp2)', color: 'white', border: 'none', cursor: 'pointer', borderRadius: '4px', padding: '0.3rem 0.7rem' }}>-</button>
       <span style={{ padding: '0 0.5rem', fontSize: '2rem'}}>{quantity}</span>
       <button onClick={handleIncrement} style={{ background: 'var(--colorp2)', color: 'white', border: 'none', cursor: 'pointer', borderRadius: '4px', padding: '0.3rem 0.7rem' }}>+</button>
