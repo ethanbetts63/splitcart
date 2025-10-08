15679e1ac81203466b6203789790ed10599c13e2
Ethan Betts <you@example.com>
2025-10-08 16:00:08 +0200

    checkpoint

--- frontend/src/components/OffCanvasMenu.jsx
+++ frontend/src/components/OffCanvasMenu.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../css/OffCanvasMenu.css';
 import ShoppingListComponent from './ShoppingListComponent';
 import StoreMap from './StoreMap';
-import LogoButton from './LogoButton';
+import cashTrolley from '../assets/cash_trolley.png'; // Import the image
 
 const OffCanvasMenu = ({ isOpen, onClose, content, onLocationChange, onStoreSelectionChange, onNavigateHome }) => {
   let title = '';
@@ -18,6 +18,17 @@ const OffCanvasMenu = ({ isOpen, onClose, content, onLocationChange, onStoreSele
 
   return (
     <div className={`off-canvas-menu-right ${isOpen ? 'visible' : ''}`}>
+      <img src={cashTrolley} alt="background" style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        objectFit: 'cover',
+        zIndex: -1,
+        opacity: 0.1,
+        maskImage: 'radial-gradient(ellipse at center, black 50%, transparent 95%)'
+      }} />
       <div className="off-canvas-header">
       </div>
       <div className="off-canvas-body">
