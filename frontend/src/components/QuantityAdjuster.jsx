6b4585a1b95e62c3b652f5d5599e3ae29ba543b2
Ethan Betts <you@example.com>
2025-10-08 15:28:20 +0200

    checkpoint

--- frontend/src/components/QuantityAdjuster.jsx
+++ frontend/src/components/QuantityAdjuster.jsx
@@ -13,9 +13,9 @@ const QuantityAdjuster = ({ quantity, onQuantityChange }) => {
 
   return (
     <div style={{ display: 'flex', alignItems: 'center' }}>
-      <button onClick={handleDecrement} style={{ background: 'var(--colorp2)', color: 'white', border: 'none', cursor: 'pointer', borderRadius: '4px', padding: '0.1rem 0.5rem' }}>-</button>
-      <span style={{ padding: '0 0.5rem', fontSize: '1.4rem' }}>{quantity}</span>
-      <button onClick={handleIncrement} style={{ background: 'var(--colorp2)', color: 'white', border: 'none', cursor: 'pointer', borderRadius: '4px', padding: '0.1rem 0.5rem' }}>+</button>
+      <button onClick={handleDecrement} style={{ background: 'var(--colorp2)', color: 'white', border: 'none', cursor: 'pointer', borderRadius: '4px', padding: '0.4rem 0.8rem' }}>-</button>
+      <span style={{ padding: '0 0.5rem', fontSize: '1.6rem' }}>{quantity}</span>
+      <button onClick={handleIncrement} style={{ background: 'var(--colorp2)', color: 'white', border: 'none', cursor: 'pointer', borderRadius: '4px', padding: '0.4rem 0.8rem' }}>+</button>
     </div>
   );
 };
