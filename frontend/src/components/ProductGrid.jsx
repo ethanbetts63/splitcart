807a978aad066ad0cffe37c79333bca73d9ef5f4
Ethan Betts <you@example.com>
2025-10-09 14:38:09 +0200

    chedkpoint

--- frontend/src/components/ProductGrid.jsx
+++ frontend/src/components/ProductGrid.jsx
@@ -3,10 +3,25 @@ import ProductTile from './ProductTile';
 
 const ProductGrid = ({ products, onLoadMore, hasMorePages, isLoadingMore, title, nearbyStoreIds }) => {
 
+  const gridStyles = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fill, minmax(18rem, 1fr))',
+    gap: '1rem',
+  };
+
+  const mediaQueryStyles = `
+    @media (max-width: 768px) {
+      .grid-container {
+        grid-template-columns: repeat(auto-fill, minmax(13.5rem, 1fr));
+      }
+    }
+  `;
+
   return (
     <div>
+      <style>{mediaQueryStyles}</style>
       {title && <h5 style={{ fontSize: '1.2rem', marginTop: '0.1rem', marginBottom: '1rem' }}>{title}</h5>}
-      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(18rem, 1fr))', gap: '1rem' }}>
+      <div className="grid-container" style={gridStyles}>
         {products.length > 0 ? (
           products.map((product) => (
             <div key={product.id}>
