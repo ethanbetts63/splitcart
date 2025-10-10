6115e2d27791b5dd5b347330213754adafa8261d
Ethan Betts <you@example.com>
2025-10-10 12:59:52 +0200

    checkpoint

--- frontend/src/components/BottomInfoCard.jsx
+++ frontend/src/components/BottomInfoCard.jsx
@@ -7,7 +7,7 @@ const BottomInfoCard = () => {
       <div className="info-item">
         <div className="info-text">
           <span className="info-title">Did you know?</span>
-          <span className="info-subtext">The average Austrlian household spends $200 (exc. alcohol) on groceries per week. Our algorithm can save you an average 10%, which means 20 bucks! That's gotta be at least a coffee! You're welcome! </span>
+          <span className="info-subtext">The average Australian household spends $200 (exc. alcohol) on groceries per week. Our algorithm can save you an average 10%, which means 20 bucks! That's gotta be at least a coffee! You're welcome! </span>
         </div>
       </div>
     </div>
