14851cf2dab7b43ddfde5a51e3f6074631c895b1
Ethan Betts <you@example.com>
2025-10-20 10:49:44 +0200

    checkpoint

--- frontend1/src/pages/LoginPage.tsx
+++ frontend1/src/pages/LoginPage.tsx
@@ -3,7 +3,7 @@ import { LoginForm } from "@/components/ui/login-form"
 export default function LoginPage() {
   return (
     <div className="container mx-auto py-10 flex justify-center">
-        <LoginForm className="w-full max-w-lg"/>
+        <LoginForm className="w-full max-w-2xl"/>
     </div>
   )
 }
