b50ee8d346e88a63fde310b8254f918fb22b25f0
Ethan Betts <you@example.com>
2025-10-29 00:12:13 +0100

    checkpoint

--- frontend/src/components/NextButton.tsx
+++ frontend/src/components/NextButton.tsx
@@ -15,15 +15,7 @@ const NextButton: React.FC<NextButtonProps> = ({ onAfterNavigate, className }) =
 
   // this should user item.substitutions to determine where to go next it should use the page that the user is on.
   const handleNextClick = () => {
-    const itemsWithSubstitutes = currentCart?.items.filter(item => 
-      item.substitutions && item.substitutions.length > 0
-    ) || [];
-
-    if (itemsWithSubstitutes.length > 0) {
-      navigate('/substitutions');
-    } else {
-      navigate('/final-cart');
-    }
+    navigate('/substitutions');
 
     if (onAfterNavigate) {
       onAfterNavigate();
