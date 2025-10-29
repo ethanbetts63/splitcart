87a66c483bd1d3cd12cebdf3b29ec6d04e094c83
Ethan Betts <you@example.com>
2025-10-29 20:21:11 +0100

    checkpoint

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  envDir: '../',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
@@ -14,9 +15,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-      },
+          target: process.env.API_SERVER_URL,
+          changeOrigin: true,
+        },
     },
   },
 })
