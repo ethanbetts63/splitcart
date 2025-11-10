176fd6a559fadcb7827bcf963c91c4344f69b4a9
Ethan Betts <you@example.com>
2025-11-10 23:10:26 +0100

    checkpoint

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -71,7 +71,7 @@ const HomePage = () => {
               They’ve been optimizing for profit. Now it’s your turn.
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Every store has a deal — but finding them all takes forever. SplitCart automates that process by comparing prices across supermarkets and splitting your cart for the cheapest overall shop. One list, multiple stores, maximum savings. Search or browse and add products to your cart to get started! Click "Next" in the bottom right when you're ready!
+              Every store has a deal — but finding them all takes forever. SplitCart automates that process by comparing prices across supermarkets and splitting your cart for the cheapest overall shop. <span className="font-bold bg-yellow-300 px-0.5 py-1 rounded italic text-black">One list, multiple stores, maximum savings.</span> Search or browse and add products to your cart to get started! Click "Next" in the bottom right when you're ready!
             </p>
           </div>
         </div>
@@ -90,12 +90,12 @@ const HomePage = () => {
           <h2 className="text-5xl font-bold tracking-tight text-gray-900 max-w-2xl mx-auto">
             What percentage of a whole chicken is bone weight?
           </h2>
-          <p className="text-xl mt-4">Short answer: <span className="font-bold">25-32%</span>. Most common answer: <span className="font-bold bg-yellow-300 px-0.5 py-1 rounded">Who cares?</span></p>
+          <p className="text-xl mt-4">Short answer: <span className="font-bold">25-32%</span>. Most common answer: <span className="font-bold bg-yellow-300 px-0.5 py-1 rounded italic text-black">Who cares?</span></p>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-12 text-lg ">
           <div className="flex flex-col gap-4">
             <p>
-              <span className="font-bold bg-yellow-300 px-0.5 py-1 rounded">I care.</span> And if you're like me then you care too. If you're like me, you've considered the dollar value of a rewards point. Or weighed the merits of ply count versus the per kilo price of toilet paper.
+              <span className="font-bold bg-yellow-300 px-0.5 py-1 rounded italic text-black">I care.</span> And if you're like me then you care too. If you're like me, you've considered the dollar value of a rewards point. Or weighed the merits of ply count versus the per kilo price of toilet paper.
             </p>
             <p className="font-bold">
               If you're like me then SplitCart was built for you. I know this to be a fact because I built SplitCart for me.
@@ -109,14 +109,14 @@ const HomePage = () => {
               I've collected the prices. I've mathematically calculated the best two, three, or four store combinations, and I've removed as much hassle as humanly possible.
             </p>
             <p>
-              So, welcome aboard fellow bargain hunter. I hope SplitCart can be as useful to you as it is to me. <span className="font-bold bg-yellow-300 px-0.5 py-1 rounded">Expect to save 10-15%</span> on your grocery bill!
+              So, welcome aboard fellow bargain hunter. I hope SplitCart can be as useful to you as it is to me. <span className="font-bold bg-yellow-300 px-0.5 py-1 rounded italic text-black">Expect to save 10-15%</span> on your grocery bill!
             </p>
             <p>
               If you have questions, suggestions or anything else I'd love to hear from you.
             </p>
             <div>
               <p>Happy hunting,</p>
-              <p className="font-bold"><span className="font-bold bg-yellow-300 px-0.5 py-1 rounded">Ethan Betts</span>, <a href="mailto:ethanbetts63@gmail.com" className="text-blue-600 hover:underline">ethanbetts63@gmail.com</a></p>
+              <p className="font-bold"><span className="font-bold bg-yellow-300 px-0.5 py-1 rounded italic text-black">Ethan Betts</span>, <a href="mailto:ethanbetts63@gmail.com" className="text-blue-600 hover:underline">ethanbetts63@gmail.com</a></p>
               <p className="italic text-sm text-gray-600">Founder and Developer</p>
             </div>
           </div>
