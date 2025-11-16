5dfe7db84f8fc6f0a35e44aa023ea93e99c3a817
Ethan Betts <you@example.com>
2025-11-16 12:19:29 +0100

    checkpoint

--- splitcart/settings.py
+++ splitcart/settings.py
@@ -150,8 +150,8 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.UserRateThrottle',
     ],
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/hour',
-        'user': '1000/hour',
+        'anon': '5000/day',
+        'user': '20000/day',
         'internal': '1000000/day',
     }
 }
