748d6a91cc321495f3bbaaabf0036d451eab4c1d
Ethan Betts <you@example.com>
2025-12-01 23:35:56 +0100

    checkpoint

--- api/views/frontend_views/product_substitute_list_view.py
+++ api/views/frontend_views/product_substitute_list_view.py
@@ -2,10 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.db.models import Q
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from products.models import Product
 from products.models.substitution import ProductSubstitution
 from ...serializers import ProductSubstitutionSerializer
 
+@method_decorator(cache_page(60 * 60 * 6), name='dispatch')
 class ProductSubstituteListView(APIView):
     def get(self, request, product_id, *args, **kwargs):
         try:
